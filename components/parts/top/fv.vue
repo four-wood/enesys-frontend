e2fbd6b6cc8a76e28bc45a0cecc79e45560b494c
kabatafw <kabata@four-wood.co.jp>
2023-01-11 16:17:19 +0900

    一時中断

--- components/parts/top/fv.vue
+++ components/parts/top/fv.vue
@@ -5,39 +5,22 @@
         <div class="fv-cover"></div>
         <div class="swiper-wrapper">
           <div v-for="slide in slides" :key="slide.id" class="swiper-slide">
-            <img
-              :src="'~/assets/img/' + slide.url"
-            />
+            <img :src="require('~/assets/img/' + slide.srcUrl)" />
           </div>
         </div>
       </div>
       <div class="fv-enesys">
-        <img src="<?php echo get_template_directory_uri()?>/img/ENESYS.svg" />
+        <img src="~/assets/img/ENESYS.svg" />
       </div>
       <div class="swiper-container-text">
         <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <a href="<?php echo home_url('/business'); ?>#solar">
-              <p class="link-text">太陽光発電ソリューション事業</p>
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a href="<?php echo home_url('/business'); ?>#solution">
-              <p class="link-text">設備ソリューション事業</p>
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a href="<?php echo home_url('/business'); ?>#retail">
-              <p class="link-text">電力販売事業</p>
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a href="<?php echo home_url('/business'); ?>#system">
-              <p class="link-text">電力見える化事業</p>
-            </a>
+          <div v-for="slide in slides" :key="slide.id" class="swiper-slide">
+            <nuxtlink :to="'/business#' + slide.query">
+              <p class="link-text">{{ slide.name }}</p>
+            </nuxtlink>
           </div>
         </div>
-        <a href="<?php echo home_url('/business'); ?>#solar" id="link-to-biz">
+        <nuxtlink :to="'/business#' + nowSlideNum" id="link-to-biz">
           <svg
             width="10"
             height="10"
@@ -50,7 +33,7 @@
               fill="#F2F2F2"
             />
           </svg>
-        </a>
+        </nuxtlink>
       </div>
       <div class="swiper-pagination-wrapper">
         <div class="swiper-pagination"></div>
@@ -61,15 +44,11 @@
         <div class="pc">
           <div class="title">
             <div class="den-hon">
-              <img
-                src="<?php echo get_template_directory_uri()?>/img/denki_pc.svg"
-                width="107"
-                height="56"
-              />
+              <img src="~/assets/img/denki_pc.svg" width="107" height="56" />
             </div>
             <img
               class="honki"
-              src="<?php echo get_template_directory_uri()?>/img/honki_pc.svg"
+              src="~/assets/img/honki_pc.svg"
               width="195"
               height="39"
             />
@@ -77,7 +56,7 @@
           <div class="text">
             <img
               class="goteian"
-              src="<?php echo get_template_directory_uri()?>/img/fv-catch_text_pc.svg"
+              src="~/assets/img/fv-catch_text_pc.svg"
               width="686"
               height="25"
             />
@@ -86,23 +65,13 @@
         <div class="sp">
           <div class="title">
             <div class="den-hon">
-              <img
-                src="<?php echo get_template_directory_uri()?>/img/denki.svg"
-              />
-              <img
-                src="<?php echo get_template_directory_uri()?>/img/denki_border.svg"
-              />
+              <img src="~/assets/img/denki.svg" />
+              <img src="~/assets/img/denki_border.svg" />
             </div>
-            <img
-              class="honki"
-              src="<?php echo get_template_directory_uri()?>/img/honki.svg"
-            />
+            <img class="honki" src="~/assets/img/honki.svg" />
           </div>
           <div class="text">
-            <img
-              class="goteian"
-              src="<?php echo get_template_directory_uri()?>/img/fv-catch_text.svg"
-            />
+            <img class="goteian" src="~/assets/img/fv-catch_text.svg" />
           </div>
         </div>
       </div>
@@ -111,24 +80,42 @@
 </template>
 <script>
 export default {
-    data() {
-        return{
-            slides:{
-                sliide1:{
-                    url:'top_fv_1.jpg'
-                },
-                sliide1:{
-                    url:'top_fv_2.jpg'
-                },
-                sliide1:{
-                    url:'top_fv_3.jpg'
-                },
-                sliide1:{
-                    url:'top_fv_4.jpg'
-                },
-            }
-        }
-        
-    },
-}
+  data() {
+    return {
+      nowSlideNum: 0,
+      slides: {
+        solar: {
+          srcUrl: "top_fv_1.jpg",
+          name: "太陽光発電ソリューション事業",
+          query: "solar",
+        },
+        solution: {
+          srcUrl: "top_fv_2.jpg",
+          name: "設備ソリューション事業",
+          query: "solution",
+        },
+        retail: {
+          srcUrl: "top_fv_3.jpg",
+          name: "電力販売事業",
+          query: "retail",
+        },
+        system: {
+          srcUrl: "top_fv_4.jpg",
+          name: "電力見える化事業",
+          query: "system",
+        },
+      },
+      swiperOptions: {
+        loop: true,
+        effetc: 'fade',
+        speed: 3000,
+        slidesPerView: "auto",
+        onSlideChangeEnd() {
+          console.log("slide changed!");
+          console.log("now slide number is " + this.NowSlideNum);
+        },
+      },
+    };
+  },
+};
 </script>
